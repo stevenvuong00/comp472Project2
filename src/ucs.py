41a27054539cf6b1aaeee7b32706e0d064422b88
amqkv <amqkvo@gmail.com>
2022-11-16 23:06:51 -0500

    Refactored ucs

--- src/ucs.py
+++ src/ucs.py
@@ -13,43 +13,50 @@ class UCS:
         self.board.printBoard()
 
         # node object: (current node, parent, cost [f(n)], total_cost[g(n)])
-        self.open.append((self.board, None, 0, 0))
+        self.open.append((self.board, None, 0))
         current_node = self.board
         total_cost = 0
+        x = 0
         goal = None
         while(len(self.open) > 0):
             # Sorting the OPEN list from lowest cost
-            sorted_open = sorted(self.open, key=lambda x: x[2])
-            current_node = sorted_open[0][0]
+            # sorted_open = sorted(self.open, key=lambda x: x[2])
+            current_node = self.open[0][0]
             current_node.getChildren()
             children = current_node.children
             total_cost += 1
+            x += 1
+            in_open = None
             for child in children:
-                isVisited = False
-                if(len(self.closed) > 0):
-                    for visited in self.closed:
-                        if(child.equals(visited[0])):
-                            isVisited = True
-                            break
-                if(isVisited):
-                    continue
-                else:
-                    child_node = (child, current_node, child.cost, total_cost)
-                    self.open.append(child_node)
+                visited = [node for node in self.closed if child.equals(node[0])]
+                if not visited:
+                    # in_open = [node for node in self.open if child.equals(node[0])]
+                    for index, node in enumerate(self.open):
+                        if(child.equals(node[0])):
+                            in_open = [node, index]
+                    if not in_open:
+                        child_node = (child, current_node, total_cost)
+                        self.open.append(child_node)
+                    else:
+                        if(in_open[0][2] > total_cost):
+                            self.open.pop(in_open[1])
+                            self.open.append((child, current_node, total_cost))
 
             # Moving the current node to CLOSED
             self.closed.append(self.open.pop(0))
             if(self.closed[-1][0].goal()):
-                goal = self.closed[-1][0]
+                goal = self.closed[-1]
                 break
             print()
+            
             current_node.printBoard()
 
             
         print()
         if(goal is not None):
-            goal.printBoard()
-            print("[cost: {}]".format(goal.cost))
+            goal[0].printBoard()
+            print("[total cost: {}]".format(goal[2]))
+            print('search length: {}'.format(x))
         else:
             print("Unsolvable noob")
         print("-------------------------------------------------------")
