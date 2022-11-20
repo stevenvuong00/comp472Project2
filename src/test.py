b55e25716ee213a3dea4bc42cc3158bd608f6530
stevenvuong00 <steven.vuong00@gmail.com>
2022-11-20 12:41:52 -0500

    implemented fuel tracking

--- src/test.py
+++ src/test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from board import Board
 from ucs import UCS
+from vehicle import Vehicle
 
 # input1 = 'BBIJ....IJCC..IAAMGDDK.MGH.KL.GHFFL.' # modified to test
 # input1 = 'BB.G.H...G.HAAMMKK..FCCIDDF..I..F...'
@@ -15,6 +16,16 @@ input5 = 'BBBBBBKKKKKKAAECZD..ECZD......FFFFFF'
 input6 = 'BBBBBBKKKKKK.AA.....ECZD..ECZDFFFFFF'
 input7 = '.............AA.....................'
 
+fuel = {'A': 99, 'M': 99}
+# board1 = Board(input1, fuel)
+# print(board1.current_fuel)
+# board1.print_board()
+# move one car down
+# board1.check_moves('M')
+# print(board1.current_fuel)
+# board1.print_board()
+# board2_test = Board(input1, board1.current_fuel)
+# print(board1.current_fuel)
 board1 = Board(input1)
 board2 = Board(input2)
 board3 = Board(input3)
@@ -23,16 +34,18 @@ board5 = Board(input5)
 board6 = Board(input6)
 board7 = Board(input7)
 
-def run_me():
-    # UCS(board1).search()
-    # UCS(board3).search()
-    print(UCS(board5).search())
-    # UCS(board2).search()
-    # UCS(board6).search()
-    # UCS(board7).search()
-    # UCS(board4).search()
-    # print(UCS(board2).search())
+# UCS(board1).search()
+# UCS(board3).search()
+print(UCS(board5).search())
+# UCS(board2).search()
+# UCS(board6).search()
+# UCS(board7).search()
+# UCS(board4).search()
+# print(UCS(board2).search())
 
-run_me()
+# car1 = Vehicle('A', board1, 99)
+# car1.print_vehicle()
+
+
 
 
--- src/ucs.py
+++ src/ucs.py
@@ -20,10 +20,9 @@ class UCS:
         start = time.time()
         print("Searching...")
 
-        # node: (current node, parent, estimate of total cost f(n), total_cost g(n), heuristic estimation h(n), move)
-        self.open.append((self.board.grid, None, 0, 0, 0, None))
+        # root node: (current node, parent, f(n), g(n), h(n), movement, fuel)
+        self.open.append((self.board.grid, None, 0, 0, 0, "", self.board.current_fuel))
         x = 0 # keep for search path length?
-        print(self.open[0])
         goal = None
 
         while len(self.open) > 0:
@@ -31,11 +30,7 @@ class UCS:
             # self.open = sorted(self.open, key=lambda k: k[2])
             # Remove first element from open
             current_node = self.open.popleft()
-            current_board = Board(current_node[0])
-
-            print("fuel ")
-            for key in current_board.vehicles.keys():
-                print(current_board.vehicles[key], current_board.vehicles[key].fuel)
+            current_board = Board(current_node[0], current_node[6])
 
             current_board.get_children()
             children = current_board.children
@@ -57,12 +52,12 @@ class UCS:
                             in_open = [node, index]
 
                     if not in_open:
-                        self.open.append((child[0], current_board, current_node[3] + 1, current_node[3] + 1, 0, child[1]))
+                        self.open.append((child[0], current_board, current_node[3] + 1, current_node[3] + 1, 0, child[1], child[2]))
                         # current_board.applied_moves = None
                     else:
                         if in_open[0][2] > current_node[3] + 1:
                             self.open.pop(in_open[1])
-                            self.open.append((child[0], current_board, current_node[3] + 1, current_node[3] + 1, 0, child[1]))
+                            self.open.append((child[0], current_board, current_node[3] + 1, current_node[3] + 1, 0, child[1], child[2]))
                             # current_board.applied_moves = None
 
         if goal is not None:
@@ -77,25 +72,33 @@ class UCS:
     def getSolutionPath(self):
         # Start with the solution and backtrack to the start state
         goal = self.closed[-1]
-        self.solution_path.append((goal[0], goal[2], goal[3], goal[4], goal[5]))
+        self.solution_path.append((goal[0], goal[5], goal[6]))
         parent = goal[1]
 
         while parent != None:
             for node in self.closed:
                 if parent.equals(node[0]):
-                    self.solution_path.append((node[0], node[2], node[3], node[4], node[5]))
+                    self.solution_path.append((node[0], node[5], node[6]))
                     parent = node[1]
                     break
         
         self.solution_path.reverse()
+        self.solution_path.pop(0)
         print("\nSolution Path: ")
         for node in self.solution_path:
-            # print(node[0])
-            print("{}\t{} {} {} {}".format(node[4], self.array_to_string(node[0]), node[1], node[2], node[3]))
-        print("Solution cost: ", len(self.solution_path) - 1, " moves")
+            # print(node)
+            print("{}\t{} {}".format(node[1], self.array_to_string(node[0]), self.process_fuel(node[2])))
+        print("Solution cost: ", len(self.solution_path), " moves")
 
     def array_to_string(self, array):
         grid = ""
         for list in array:
             grid += "".join(list)
         return grid
+
+    def process_fuel(self, fuel):
+        fuel_str = ""
+        for key in fuel.keys():
+            if fuel[key] != 100:
+                fuel_str = fuel_str + key + str(fuel[key]) + " "
+        return fuel_str
